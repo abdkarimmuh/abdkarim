ed0dec648fac249a4234801f9e2aace0b5689cf8
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2026-06-17 21:58:43 +0700

    add template once ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,62 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.{ts,tsx}"],
+
+    plugins: {
+      import: importPlugin,
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/set-state-in-effect": "off",
+
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["\\.css$"],
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
+import mdx from "@next/mdx";
+
 import type { NextConfig } from "next";
 
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  transpilePackages: ["next-mdx-remote"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "**",
+      },
+    ],
+  },
+  sassOptions: {
+    compiler: "modern",
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
